c16db044b2f16d6da665f9668ca9b45d8cff2834
William PETIT <william.petit@ac-dijon.fr>
2014-01-21 17:29:55 +0100

    Utilisation de restify

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-// Start sails and pass it command line arguments
-require('sails').lift(require('optimist').argv);
+var restify = require('restify');
+var config = require('./lib/util/config');
+
+var serverConf = config.get('server');
+
+var server = restify.createServer(serverConf);
+
+require('./lib/middlewares')(server);
+require('./lib/routes')(server);
+
+server.listen(serverConf.port);
