ebd3792ddde81a6bf0e63da83f1918ab50956a63
Will <william.petit@lookingfora.name>
2014-05-28 22:22:23 +0200

    Logique de base ticker

---
+++ ticker.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var config = require('./lib/util/config');
+var TurnManager = require('./lib/util/turn-manager');
+
+mongoose.connect(
+  config.get('database:uri'),
+  config.get('database:options')
+);
+
+TurnManager.start();
